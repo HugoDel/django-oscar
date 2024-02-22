c6cc3fda4e9959ab47a1f50fe20cea620f575f52
Hugo <delannoy_45@msn.com>
2024-02-22 07:34:56 +0100

    SCSS: remove link underline Template (bulk): rename input-group to d-flex, form-group to input-group, text[left|right] to text-[start|end], m[r,l,...]* to m[e,s,...]*

--- gulpfile.js/subtasks/scss.js
+++ gulpfile.js/subtasks/scss.js
@@ -1,5 +1,5 @@
 const gulp = require('gulp');
-const sass = require('gulp-sass')(require('sass'));
+const sass = require('gulp-dart-sass');
 const sourcemaps = require('gulp-sourcemaps');
 const pump = require('pump');
 
